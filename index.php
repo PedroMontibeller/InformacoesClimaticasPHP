6355bc5006bcd92eb689688c6e11b2df9fa09f6e
Aluno <Aluno@sn626>
2019-11-29 21:52:07 -0200

    Adicionado tags gerais e para facebook

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Projeto Climar</title>
+        <title>Projeto Clima SENAI</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="js/clima.js"></script>
+        <meta name="DESCRIPTION" content="Projeto de informações climáticas de Brusque desenvolvido por Pedro Henrique Montibeller no SENAI-SC"/>
+        <meta name="ABSTRACT" content="Informações climáticas"/>
+        <meta name="keywords" content="projeto senai, temperatura de brusque, tempo em brusque, clima de brusque, por do sol de brusque, anoitecer de brusque, pressão de brusque, pedro montibeller, temperatura de brusque, umidade de brusque, maxima de brusque, clima, tempo, situação de brusque"/>
+        <meta name="title" content="Projeto Clima SENAI" />
+        <meta name="identifier-url" content="https://pedro-info-climaticas.herokuapp.com" />
+        <meta name="author" content="Pedro Henrique Montibeller" />
+        <meta name="ROBOTS" content="All" />
+        <meta name="RATING" content="general" />
+        <meta name="DISTRIBUTION" content="global" />
+        <meta name="LANGUAGE" content="pt-br" />
+        <meta name="content-language" content="portuguese" />
+        <meta name="doc-class" content="Completed" /> 
+        <meta name="reply-to" content="pedro_montibeller@estudante.sc.senai.br"/>
+        <meta property="og:url" content="https://pedro-info-climaticas.herokuapp.com" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Projeto Clima SENAI" />
+        <meta property="og:title" content="Projeto Clima SENAI" />
+        <meta property="og:description" content="Projeto de informações climáticas de Brusque desenvolvido por Pedro Henrique Montibeller no SENAI-SC" />
+        <meta property="og:image" content="https://pedro-info-climaticas.herokuapp.com/img/icones/tMin.png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="120" />
+        <meta property="fb:app_id" content="" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Projeto de informações climáticas de Brusque desenvolvido por Pedro Henrique Montibeller no SENAI-SC" />
+        <meta name="twitter:title" content="Projeto Clima SENAI" />
     </head>
     <body>
         <div class="alert alert-danger mt-3">
